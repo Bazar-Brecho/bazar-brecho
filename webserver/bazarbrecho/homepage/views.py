acef998793b53ee79702dd054eda7a72fdd0d753
Alexandre Barbosa Bruno <alexandrebbruno@gmail.com>
2022-10-16 10:12:07 -0300

    add black and fix-format (#1)
    * add environment.yml, requirements.txt, pre-commit
    
    * fix format changes
    
    * update readme

--- webserver/bazarbrecho/homepage/views.py
+++ webserver/bazarbrecho/homepage/views.py
@@ -4,25 +4,25 @@ from .models import product_entry
 
 
 def home(request):
-    return render(request, 'homepage.html')
+    return render(request, "homepage.html")
 
 
 def login(request):
-    return render(request, 'user_login.html')
+    return render(request, "user_login.html")
 
 
 def clothes_list(request):
     all_clothes = product_entry.objects.all()
-    return render(request, 'clothes_list.html', {'all_items': all_clothes})
+    return render(request, "clothes_list.html", {"all_items": all_clothes})
 
 
 def add_item(request):
-    new_item = product_entry(content = request.POST['content'])
+    new_item = product_entry(content=request.POST["content"])
     new_item.save()
-    return HttpResponseRedirect('/clothes_list/')
+    return HttpResponseRedirect("/clothes_list/")
 
 
 def delete_item(request, item_id):
     del_item = product_entry.objects.get(id=item_id)
     del_item.delete()
-    return HttpResponseRedirect('/clothes_list/')
+    return HttpResponseRedirect("/clothes_list/")
