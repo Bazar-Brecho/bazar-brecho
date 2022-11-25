55061305f735350fea35043a5c7ff8bea39d89b7
unknown <ra00168429@pucsp.edu.br>
2022-11-24 21:48:25 -0300

    inclusao do link de descrisao no nome e na imagem

--- webserver/bazarbrecho/homepage/models.py
+++ webserver/bazarbrecho/homepage/models.py
@@ -26,10 +26,9 @@ class ProductEntry(models.Model):
 
 
 class ClienteEntry(models.Model):
-    product_image = models.ImageField(upload_to="templates/roupas", default="")
-    product_name = models.CharField(max_length=60)
-    product_size = models.CharField(max_length=2, choices=PRODUCT_SIZES)
-    product_price = models.DecimalField(decimal_places=2, max_digits=30)
+    client_image = models.ImageField(upload_to="templates/roupas", default="")
+    client_name = models.CharField(max_length=60)
+    client_size = models.CharField(max_length=2, choices=PRODUCT_SIZES)
 
 
 class Customer(models.Model):
